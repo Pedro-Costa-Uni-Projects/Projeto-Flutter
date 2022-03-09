1c219bd9b0577eda133cd9c62cb2d11fe68dd99e
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-03-09 03:49:22 +0000

    Drawer base created, (moodle is down remember to download project pdf)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,9 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const Main());
 
-class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+class Main extends StatelessWidget {
+  const Main({Key? key}) : super(key: key);
 
   // This widget is the root of your application.
   @override
@@ -13,53 +11,23 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const Dashboard(title: 'Mini-Projeto CD - Mudar nome'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+class Dashboard extends StatefulWidget {
+  const Dashboard({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<Dashboard> createState() => _DashboardState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+class _DashboardState extends State<Dashboard> {
 
   @override
   Widget build(BuildContext context) {
@@ -76,40 +44,71 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: Text("Teste"),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Center(
+                child: Text("Teste - Meter Aqui Logo"),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Item 1'),
+                //subtitle: Text('Subtitulo Item 1'),
+                onTap: () {
+                  /*
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HistoricScreen()),
+                  );
+                  */
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Item 2'),
+                //subtitle: Text('Subtitulo Item 2'),
+                onTap: () {
+                  /*
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HistoricScreen()),
+                );
+                */
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            Card(
+              child: ListTile(
+                leading: Icon(Icons.add),
+                title: Text('Item 3'),
+                //subtitle: Text('Subtitulo Item 3'),
+                onTap: () {
+                  /*
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => HistoricScreen()),
+                  );
+                  */
+                  Navigator.pop(context);
+                },
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
