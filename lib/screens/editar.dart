56a1215b4cb0e630a109f4433824b6e751e42039
pedroC-21904825 <a21904825@alunos.ulht.pt>
2022-03-10 18:45:44 +0000

    overfloading bottom, added "resizeToAvoidBottomInset: false"

--- lib/screens/editar.dart
+++ lib/screens/editar.dart
@@ -28,9 +28,8 @@ class _EditarScreenState extends State<EditarScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Column(
